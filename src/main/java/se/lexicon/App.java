9438c2dc43231e6500c68ed6d0dac5cc396174e1
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-13 15:48:33 +0200

    ex3

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+public class App {
+
+    public static void main(String[] args) {
+        // ex1
+        //System.out.println( "Hello azadeh!" );
+
+
+
+        // ex2
+        /*int number1 = 45;
+        int number2 = 11;
+        int sum = number1 + number2;
+        System.out.println("Result:" + sum);*/
+
+        // ex3
+        int number1 = 3;
+        int number2 = 5;
+        int multiply = number1 * number2;
+        System.out.println("Result:" + multiply);
+    }
+}
