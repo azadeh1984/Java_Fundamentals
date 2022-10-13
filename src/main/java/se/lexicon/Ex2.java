107a5dbb24c0c6e1fb571991549d327689938ed4
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-13 15:45:05 +0200

    ex2

--- src/main/java/se/lexicon/Ex2.java
+++ src/main/java/se/lexicon/Ex2.java
@@ -3,6 +3,10 @@ package se.lexicon;
 public class Ex2 {
 
     public static void main(String[] args) {
-
+        int number1 = 3;
+        int number2 = 5;
+        int multiply = number1 * number2;
+        System.out.println("Result:" + multiply);
     }
+
 }
