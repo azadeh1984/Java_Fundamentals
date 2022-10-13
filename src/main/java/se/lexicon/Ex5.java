051c1cbe949fa2e8c729db7e5d66a4f7397cdb2b
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-13 16:47:36 +0200

    ex5

---
+++ src/main/java/se/lexicon/Ex5.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int num1 = 55;
+        int  num2 = 12;
+        int minus = num1 - num2;
+        System.out.println("Result"+ minus);
+    }
+}
