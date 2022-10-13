98f98ac0fb10acef2d89ba16ea5fc50ee32f7589
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-13 16:39:27 +0200

    ex4

---
+++ src/main/java/se/lexicon/Ex4.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int num1 = 24;
+        int num2 = 6;
+        int division = num1/num2;
+        System.out.println("Result"+ division);
+    }
+}
