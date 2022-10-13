bb497e61e540d230a42d423dc4a98a1a37708712
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-13 15:45:46 +0200

    ex3

---
+++ src/main/java/se/lexicon/Ex3.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+import javax.xml.transform.Result;
+
+public class Ex3 {
+
+    public static void main(String[] args) {
+        int number1 = 45;
+        int number2 = 11;
+        int sum = number1 + number2;
+        System.out.println("Result:" + sum);
+
+
+    }
+
+}
+
+
+
